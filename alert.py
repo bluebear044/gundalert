5dfb2c85d5f4f9f5650ca6469d03ce93a35aefed
bluebear044 <bluebear044@gmail.com>
2023-07-01 22:30:17 +0900

    schedule 라이브러리 사용 시 try exception 적용

--- alert.py
+++ alert.py
@@ -1,9 +1,12 @@
 import logging
 import traceback
+import configparser
 from db import DB
 
 class Alert:
 
+    config = configparser.ConfigParser()
+    config.read("config.ini")
     logging.basicConfig(filename="gundalert.log", encoding="utf-8", level=logging.INFO)
 
     def newItemAlert(self):
@@ -12,6 +15,10 @@ class Alert:
             rows = db.selectByAlertFlag()
             for item in rows: 
                 logging.info(item)
+
+            # todo : alert_flag값이 false인 것 (알람이 필요한 것)에 대해 메일 전송 후 update_flag값 true로 업데이트하기
+            # message += ' '.join(str(value) for value in item.values()) + "\n"
+            # sendEmail(config.get("Email", "sender"), config.get("Email", "password"), config.get("Email", "receiver"), "gundalert : " + datetime.datetime.now().strftime("%Y%m%d%H%M%S"), message)
             
         except Exception as e:
             logging.error(traceback.print_exc())
--- soup_auto.py
+++ soup_auto.py
@@ -1,6 +1,9 @@
 import schedule
 import time
+import logging
 from soup import Soup
+
+logging.basicConfig(filename="schedule.log", encoding="utf-8", level=logging.DEBUG)
 
 def call_func():
     soup = Soup()
@@ -8,8 +11,10 @@ def call_func():
     soup.bnkrMallSearchSoup()
     soup.gundamBoomSoupPaging()
 
-schedule.every(5).seconds.do(call_func)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+try:
+    schedule.every(5).seconds.do(call_func)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+except Exception as e:
+    logging.error(traceback.print_exc())
