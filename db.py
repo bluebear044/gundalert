5add61caf1a0ffb5651b04b9d07353b2ff692d90
bluebear044 <bluebear044@gmail.com>
2023-07-01 22:49:01 +0900

    DB pool 적용

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import psycopg2
+from psycopg2 import pool
 import logging
 import configparser
 from datetime import datetime
@@ -8,19 +9,26 @@ config = configparser.ConfigParser()
 config.read("config.ini")
 logging.basicConfig(filename="gundalert.log", encoding="utf-8", level=logging.INFO)
 
+connection_pool = psycopg2.pool.SimpleConnectionPool(minconn=1,
+                                                maxconn=5,
+                                                user=config.get("DB", "user"),
+                                                password=config.get("DB", "password"),
+                                                host=config.get("DB", "host"),
+                                                port=config.get("DB", "port"),
+                                                database=config.get("DB", "database"))
+
+def getConnection():
+    return connection_pool.getconn()
+
+
+def releaseConnection(conn):
+    connection_pool.putconn(conn)
+
 class DB:
-
-    def getConnection(self):
-        return psycopg2.connect(user=config.get("DB", "user"),
-                                    password=config.get("DB", "password"),
-                                    host=config.get("DB", "host"),
-                                    port=config.get("DB", "port"),
-                                    database=config.get("DB", "database"))
-
 
     def insert(self, url, title, price):
         try:
-            connection = self.getConnection()
+            connection = getConnection()
             cursor = connection.cursor()
 
             query = "INSERT INTO gund_item (url, title, price, soup_count, reg_dttm) VALUES (%s,%s,%s,1,CURRENT_TIMESTAMP) ON CONFLICT (url) DO UPDATE SET soup_count = gund_item.soup_count + 1, mod_dttm = CURRENT_TIMESTAMP"
@@ -35,15 +43,15 @@ class DB:
             logging.error("Failed to insert record into mobile table %s",error)
 
         finally:
-            if connection:
+            if connection is not None:
                 cursor.close()
-                connection.close()
+                releaseConnection(connection)
                 logging.debug("PostgreSQL connection is closed")
 
 
     def select(self):
         try:
-            connection = self.getConnection()
+            connection = getConnection()
             cursor = connection.cursor()
 
             query = "SELECT * FROM gund_item"
@@ -55,15 +63,15 @@ class DB:
             logging.error("Failed to insert record into mobile table %s",error)
 
         finally:
-            if connection:
+            if connection is not None:
                 cursor.close()
-                connection.close()
+                releaseConnection(connection)
                 logging.debug("PostgreSQL connection is closed")
 
 
     def selectByAlertFlag(self):
         try:
-            connection = self.getConnection()
+            connection = getConnection()
             cursor = connection.cursor()
 
             query = "SELECT * FROM gund_item WHERE alert_flag = 'false'"
@@ -75,9 +83,9 @@ class DB:
             logging.error("Failed to insert record into mobile table %s",error)
 
         finally:
-            if connection:
+            if connection is not None:
                 cursor.close()
-                connection.close()
+                releaseConnection(connection)
                 logging.debug("PostgreSQL connection is closed")
 
 
